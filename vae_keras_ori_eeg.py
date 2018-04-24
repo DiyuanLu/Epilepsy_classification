da24d7488e28cad5855b9fc71edb74ce61cf45e5
DiyuanLU <elu@users.fias.science>
2018-04-24 14:30:31 +0200

    wow! keras eeg, works!! the loss looks weird tho

--- vae_keras_ori_eeg.py
+++ vae_keras_ori_eeg.py
@@ -9,6 +9,7 @@ from keras import objectives
 from keras import backend as K
 import datetime
 import ipdb
+from tensorflow.python import debug as tfdb          ## for debuging
 from keras.models import model_from_json
 import functions as func
 
@@ -32,9 +33,9 @@ seq_shape = (original_dim, channel)
 intermediate_dim = 256
 latent_dim = 2
 
-nb_epochs = 2
+nb_epochs = 30
 epsilon_std = 1.0
-data_dir = "data/npy_data/ds_8testF751testN1501_average.npz"
+data_dir = "data/npy_data/testF751testN1501_aver_zscore.npz"
 
 datetime = "{0:%Y-%m-%dT%H-%M-%S}".format(datetime.datetime.now())
 results_dir= "results/vae-keras-ori-eeg/".format(batch_size)+ datetime
@@ -43,12 +44,14 @@ if not os.path.exists(logdir):
     os.makedirs(logdir)
 if not os.path.exists(results_dir):
     os.makedirs(results_dir)
-    
+print results_dir
+
+#sess = K.get_session()
+#sess = tfdb.LocalCLIDebugWrapperSession(sess)
+#K.set_session(sess)
 ################## Encoder ##################
 X = Input(batch_shape = (batch_size, original_dim*channel))
-print "X", X
 net  =  Dense(intermediate_dim, activation = 'relu')(X)
-print "enc", net
 z_mean = Dense(latent_dim)(net)
 z_log_var = Dense(latent_dim)(net)
 z = Lambda(sampling, output_shape = (latent_dim,))([z_mean, z_log_var])
@@ -58,15 +61,13 @@ h_decoder = Dense(intermediate_dim, activation = 'relu')
 X_bar = Dense(original_dim * channel, activation = 'sigmoid')
 # we instantiate these layers separately so as to reuse them later
 dec_net = h_decoder(z)
-print "dec_h", dec_net 
 X_reconstruction  = X_bar(dec_net )
-print "X_decoded", X_reconstruction
 
 ############ loss ######################3
-def vae_loss(x,x_rec):
-    reconst_loss = original_dim*objectives.binary_crossentropy(x, x_rec)
+def vae_loss(x, x_rec):
+    reconst_loss = objectives.binary_crossentropy(x, x_rec)
     kl_loss = -0.5*K.sum(1 + z_log_var - K.square(z_mean) - K.exp(z_log_var), axis = -1)
-    return reconst_loss - kl_loss
+    return reconst_loss + kl_loss
 # build and compile model
 vae = Model(X , X_reconstruction)
 vae.compile(optimizer = 'adam', loss = vae_loss)
@@ -84,18 +85,19 @@ x_test = x_test.astype('float32')
 x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
 x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
 
+
 # x_train is required for input and loss output as target
-vae.fit(x_train,x_train, shuffle = True, epochs = nb_epochs, batch_size = batch_size, validation_data = (x_test, x_test))
+vae.fit(x_train, x_train, shuffle = True, epochs = nb_epochs, batch_size = batch_size, validation_data = (x_test, x_test))
 
-# encoder is the inference network
+############### encoder is the inference network
 encoder = Model(X, z_mean)
-
-ipdb.set_trace()
-# a 2d plot of 10 digit classes in latent space
 x_test_encoded = encoder.predict(x_test, batch_size = batch_size)
+x_decoded = generator.predict(z_sample)
 plt.figure(figsize = (6,6))
-plt.scatter(x_test_encoded[:,0], x_test_encoded[:,1], c = y_test)
+plt.scatter(x_test_encoded[:,0], x_test_encoded[:,1], c = np.argmax(y_test, axis=1))
 plt.colorbar()
+plt.savefig(results_dir + '/latent_scatter.png', format='png')
+plt.close()
 
 
 # since the generator treats z as an input, we make z an input layer
@@ -107,27 +109,49 @@ generator =  Model(z_input, _x_decoded)
 #####plot sample reconstruction
 z_sample = np.array([np.random.normal(0, 1, latent_dim)])
 print z_sample
-x_decoded = generator.predict(z_sample)
+x_decoded = generator.predict(z_sample)      ### start from random
+x_test_recon = generator.predict(x_test_encoded[0:5, :])      # use the encoding from test reconstruct
 sampled_im = x_decoded[0].reshape(original_dim, channel )
 plt.figure()
-plt.imshow(sampled_im, cmap = 'Greys_r')
+plt.plot(sampled_im, 'c', label='rand_latent_recon')
+plt.legend()
+plt.savefig(results_dir + '/sampled_reconstructon.png', format='png')
+plt.close()
 
+ipdb.set_trace()
+plt.figure()
+for ii in range(5)
+    ax1 = plt.subplot(5,1, ii +1)  
+    plt.plot(x_test_recon[ii:], label='test_recon_{}'.format(ii))
+    plt.plot(x_test[ii:], label='test_original'.format(ii))
+plt.legend()
+plt.savefig(results_dir + '/test_reconstructons.png', format='png')
+plt.close()
 
 #####plot prior
 # 2d manifold of images by exploring quantiles of normal dist (using the inverse of cdf)
-n = 15
-figure  =  np.zeros((2*n, original_dim))
-
+n = 3
+figure  =  plt.subplot()
 grid_x = norm.ppf(np.linspace(0.05,0.95,n))
 grid_y = norm.ppf(np.linspace(0.05,0.95,n))
 #latent = np.random.normal(0, 1, latent_dim)
+gs = plt.GridSpec(n, n, hspace=0)
+fig = plt.figure()
+other_axes = [fig.add_subplot(gs_top[i,:], sharex=ax) for i in range(1: )]
+other_axes[0].plot(data12, 'c', label="non-focal")
+
+
 for i, yi in enumerate(grid_x):
     for j,xi in enumerate(grid_y):
         latent =  np.array([[xi, yi]]) 
         x_decoded = generator.predict(z_sample)
-        digit = x_decoded[0].reshape(original_dim, )
-        figure[i*digit_size:(i+1)*digit_size, 
-              j*digit_size:(j+1)*digit_size] = digit
+        if i == 0 and j == 0:
+            ax = fig.add_subplot(gs[0, :])
+            ax.plot(x_decoded)
+            plt.setp(ax.get_xticklabels(), visible=False)
+        else:
+            other_axes[i + j].plot(x_decoded)
+            
         
 plt.figure(figsize = (10,10))
 plt.imshow(figure, cmap = 'Greys_r')
@@ -158,15 +182,12 @@ loaded_model.compile(loss = 'binary_crossentropy', optimizer = 'rmsprop', metric
 score = loaded_model.evaluate(X, Y, verbose = 0)
 print("%s: %.2f%%" % (loaded_model.metrics_names[1], score[1]*100))
 '''
-Epoch 1/50
-60000/60000 [==============================] - 8s 130us/step - loss: 201.0283 - val_loss: 174.7658
-Epoch 15/50
-60000/60000 [==============================] - 8s 128us/step - loss: 153.9201 - val_loss: 154.6933
-Epoch 25/50
-60000/60000 [==============================] - 8s 129us/step - loss: 150.8378 - val_loss: 152.4690
-Epoch 40/50
-60000/60000 [==============================] - 8s 129us/step - loss: 148.3840 - val_loss: 151.0905
-Epoch 45/50
-60000/60000 [==============================] - 8s 134us/step - loss: 147.8082 - val_loss: 150.8328
-Epoch 50/50
-60000/60000 [==============================] - 8s 126us/step - loss: 147.3580 - val_loss: 150.6444'''
+Epoch 1/20
+5955/5955 [==============================] - 6s 950us/step - loss: 40622.3800 - val_loss: 2549816.5934
+Epoch 5/20
+5955/5955 [==============================] - 5s 911us/step - loss: 9.0785 - val_loss: 167811.6405
+Epoch 15/20
+5955/5955 [==============================] - 5s 896us/step - loss: 6.6307 - val_loss: 116943.6244
+Epoch 20/20
+5955/5955 [==============================] - 5s 918us/step - loss: 6.1745 - val_loss: 145969.5441
+'''
